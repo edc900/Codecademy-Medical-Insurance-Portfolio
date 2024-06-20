1c3c58ab8908aab903c993356e2ece7247742cd6
edc900 <edc900@gmail.com>
2024-06-20 09:01:03 -0400

    add process for organizing insurance.csv file into a python dictionary

--- analysis.py
+++ analysis.py
@@ -16,12 +16,48 @@ How does age relate to whether someone is a smoker or not?
 
 """
 
+
+
+def categorize(data_list,categories,conditions):
+    group = []
+    for row in data_list:
+        for category in range(len(categories)):
+            continue
+    return group
+
+
+
 # opens insurance.csv to the reader object to be analyzed
 with open('insurance.csv',newline="") as insurance:
     reader = csv.reader(insurance, delimiter=",")
+
+    # Creates an empty dictionary for sorting the csv data by column.
+    insurance_data = {}
+
+    # Iterates through each row of the csv file.
     line = 0
     for row in reader:
-        if line == 10:
-            break
-        print(row)
+        # For the first row of the csv file (which will be the headers): 
+        #   make the header a key in the insurance_data dictionary,
+        #   and initialize the key with an empty list (for storing data from each column).
+        if line == 0:
+            for header in row:
+                insurance_data[header] = []
+        
+        # For every row of data:
+        #   find the key associated with the column in the insurance_data dictionary,
+        #   and append the data to the empty list associated with the header key.
+        else:
+             headers = list(insurance_data.keys())
+             for index in range(len(row)):
+                 insurance_data[headers[index]].append(row[index])
         line+=1
+    
+    # The insurance_data dictionary should now be in a dataframe-ready format (if we were using the Pandas library):
+    #   The headers from the csv file are now the keys in the insurance_data dictionary,
+    #   and the data from each row in insurance.csv are stored in indexed lists for each header key
+
+    
+
+    print(insurance_data.keys())
+    
